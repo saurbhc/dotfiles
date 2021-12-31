c5434b55827fae6dddd8a4a2cc367e7dfc91aeea
Saurabh Chopra <Saurabh.Chopra.2021@live.rhul.ac.uk>
2021-12-31 15:47:00 +0000

    install_node.sh updated

--- install_node.sh
+++ install_node.sh
@@ -2,5 +2,8 @@
 # git link: https://github.com/nvm-sh/nvm#installing-and-updating
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
+# intermediate step
+source ~/.bashrc
+
 # 2. install lts version of node, this will also install npm
 nvm install --lts
