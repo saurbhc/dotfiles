723af41c68c5342041c99e1146d9dc0a6f823f30
Saurabh Chopra <Saurabh.Chopra.2021@live.rhul.ac.uk>
2022-01-01 00:57:06 +0000

    install_pyenv.sh updated

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -3,6 +3,8 @@
 # https://github.com/pyenv/pyenv/wiki/Common-build-problems
 
 # install prerequisite packages for Ubuntu
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
 # Install
