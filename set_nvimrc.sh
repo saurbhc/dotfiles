6cfa6b718a19b5e893eb73a69f373b33ba669830
Saurabh Chopra <saurabh.chopra@x-chemrx.com>
2022-12-04 22:04:54 +0000

    update set_nvimrc.sh -- installing vim-plug

--- set_nvimrc.sh
+++ set_nvimrc.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# create .config dir if not exists
-if [ ! -d "~/.config" ]
-then
-    mkdir ~/.config
-fi
-
 # create .config/nvim dir if not exists
-if [ ! -d "~/.config/nvim" ]
-then
-    mkdir ~/.config/nvim
-fi
+mkdir -p ~/.config/nvim
+
+# Install vim-plug
+# Docs: https://github.com/junegunn/vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # copy the init.vim file
 cp .config/nvim/init.vim ~/.config/nvim/init.vim
