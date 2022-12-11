f4a99f15ccaccc6b3b0ca910c1ade8bab6eaf34f
saurbhc <Saurabh.Chopra.2021@live.rhul.ac.uk>
2022-12-11 23:06:35 +0000

    Update install_nvim.sh

--- install_nvim.sh
+++ install_nvim.sh
@@ -10,6 +10,9 @@ then
     sudo apt-get install -y neovim
     echo "installing vimplug.."
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    
+    echo "installing python extensions"
+    python3 -m pip install --upgrade pynvim jedi
 elif [ "$(uname)" == "Darwin" ]
 then
     echo "NotImplemented for Darwin yet..."
